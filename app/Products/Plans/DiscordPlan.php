7496796f5451db0d270b1eb9ff587e08b6ee23d2
Colby McHenry <colbymchenry@Colbys-MBP.attlocal.net>
2020-04-23 17:31:11 -0400

    Caching fix.

--- app/Products/Plans/DiscordPlan.php
+++ app/Products/Plans/DiscordPlan.php
@@ -2,12 +2,23 @@
 
 namespace App\Products\Plans;
 
+use Illuminate\Support\Facades\Cache;
+
 class DiscordPlan extends Plan
 {
 
     public function update(\Illuminate\Http\Request $request)
     {
         
+    }
+
+    public function create(\Illuminate\Http\Request $request)
+    {
+        try {
+            parent::create($request);
+            $key = 'price_' . $this->product->getStripeID() . '_' . $this->interval_cycle;
+            Cache::put($key, $request['price'], 60 * 5);
+        } catch(\Exception $e) {}
     }
 
     public function getStripeID(): string
