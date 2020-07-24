c3489852f6a71894d20cc225b7b0314084416afe
Colby McHenry <colbymchenry@Colbys-MBP.attlocal.net>
2020-07-24 07:13:16 -0400

    Setup BeastlyBot on home system.

--- config/app.php
+++ config/app.php
@@ -227,7 +227,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'SiteConfig' => App\SiteConfig::class
-
     ],
 
 ];
