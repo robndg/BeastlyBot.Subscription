00aab1517777f0b641753388e70802cb1e7f6e81
Robs Studio <me@robs.studio>
2021-04-16 22:50:39 +0000

    Store set Live = 1 when Processor Added (Stripe)

--- app/Http/Controllers/StripeConnectController.php
+++ app/Http/Controllers/StripeConnectController.php
@@ -47,6 +47,7 @@ class StripeConnectController extends Controller
             // Set all stores to processor_id
             $discord_o_auth = DiscordOAuth::where('user_id', $user->id)->first();
             $user_discord_stores = DiscordStore::where('user_id', $discord_o_auth->discord_id)->update(['processor_id' => $processorConnect->id]);
+            $user_discord_stores = DiscordStore::where('user_id', $discord_o_auth->discord_id)->update(['live' => 1]); // TODOv2: add toggle
            // $user_discord_stores->save();
 
             /*$user->StripeConnect->express_id = $stripe_account->id;
@@ -54,6 +55,13 @@ class StripeConnectController extends Controller
             AlertHelper::alertSuccess('Stripe account created! You can now accept payments.');
             return redirect('/dashboard');
         } else {
+            // Set all stores to processor_id
+            $discord_o_auth = DiscordOAuth::where('user_id', $user->id)->first();
+           
+            $processorConnect_id = Processors::where('user_id', $user->id)->where('type', 1)->first()->id; 
+            // TODOv3: make stores allow diff processors, ie. paypal 
+            $user_discord_stores = DiscordStore::where('user_id', $discord_o_auth->discord_id)->update(['processor_id' => $processorConnect_id]);
+            $user_discord_stores = DiscordStore::where('user_id', $discord_o_auth->discord_id)->update(['live' => 1]); // TODOv2: add toggle
             AlertHelper::alertInfo('You have already linked a Stripe account.');
             return redirect('/dashboard');
         }
